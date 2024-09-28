16402d491d3b3c279797bb72456dd7683b926a5b
Karasevich177 <kowbasyuck.kirill@yandex.ru>
2024-09-28 15:47:22 +0300

    coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,11 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    // Hello
+    /*/
+     * bye
+     * bye
+     /*/
     void Start()
     {
         Time.timeScale = _timeScale;
